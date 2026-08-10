35df2f8dab3b822630c1d670e5a37067f154598a
ElenaWebDev984 <badalova.dev.984@gmail.com>
2026-08-10 13:02:53 +0200

    update deleteTodolistAC

--- src/model/todolists-reducer.ts
+++ src/model/todolists-reducer.ts
@@ -1,5 +1,17 @@
 import {v1} from 'uuid'
 import type {FilterValues, Todolist} from '../app/App.tsx'
+import { createAction } from "@reduxjs/toolkit"
+
+export const deleteTodolistAC = createAction<{id: string}>('todolists/deleteTodolist')
+// export const deleteTodolistAC = (id: string) => {
+//   return {type: 'delete_todolist', payload: { id }} as const
+// }
+export const createTodolistAC = (title: string) => {
+  return {type: 'create_todolist', payload: { title, id: v1() }} as const
+}
+// export const createTodolistAC = (title: string) => {
+//   return {type: 'create_todolist', payload: { title, id: v1() }} as const
+// }
 
 const initialState: Todolist[] = []
 
@@ -23,13 +35,6 @@ export const todolistsReducer = (state: Todolist[] = initialState, action: Actio
   }
 }
 
-export const deleteTodolistAC = (id: string) => {
-  return {type: 'delete_todolist', payload: { id }} as const
-}
-
-export const createTodolistAC = (title: string) => {
-  return {type: 'create_todolist', payload: { title, id: v1() }} as const
-}
 
 export const changeTodolistTitleAC = (payload: {id: string, title: string}) => {
   return {type: 'change_todolist_title', payload} as const
